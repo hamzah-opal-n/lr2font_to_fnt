50d9a4a1a3acbf1b238138061a6868a962ff58b2
Hamzah "Opal" Nutt <hamzah.nutt@my.jcu.edu.au>
2026-07-09 14:13:08 +0800

    Create new converter function, currently a duplicate of the old function

--- lr2font_to_fnt.py
+++ lr2font_to_fnt.py
@@ -3,15 +3,17 @@ import subprocess
 
 
 def convert_glyph_id(glyph_id):
-    if glyph_id >= 256 and glyph_id <= 8126:
-        glyph_id += 32832
-    elif glyph_id >= 8127 and glyph_id <= 15306:
-        glyph_id += 49281
+    if not (glyph_id < 256 or glyph_id > 15306):
+        if glyph_id >= 8127:
+            glyph_id += 49281
+        else:
+            glyph_id += 32832
     glyph_id = glyph_id.to_bytes((glyph_id.bit_length() + 7) // 8, 'big')
     return ord(glyph_id.decode('cp932'))
 
 
 def resize(thing, scale):
+    # TODO: change this to return integer values rather than strings
     thing = round(float(thing) * scale)
     return str(thing)
 
@@ -78,6 +80,68 @@ def old_converter(fileName, scale):
         print("ImageMagick not found. You'll have to convert the textures to .png yourself I guess...")
 
 
+def convert_lr2font_to_fnt(lr2font_filename, scale):
+    # LOAD LR2FONT AND SORT TAGS
+    input_file = open(os.path.expanduser(lr2font_filename), "r", errors="ignore")
+    list_s = []
+    list_m = []
+    list_t = []
+    list_r = []
+    output = []
+    font_name = lr2font_filename.replace(".lr2font", "")
+
+    while True:
+        line = input_file.readline()
+        if not line:
+            break
+        if line[0:2] == "#S":
+            list_s.append(line.strip().split(","))
+        elif line[0:2] == "#M":
+            list_m.append(line.strip().split(","))
+        elif line[0:2] == "#T":
+            list_t.append(line.strip().split(","))
+        elif line[0:2] == "#R":
+            list_r.append(line.strip().split(","))
+    input_file.close()
+
+    # INFO
+    line = "info face=\"" + font_name + "\" size=" + resize(
+        list_s[-1][1],
+        scale) + " bold=0 italic=0 charset=\"\" unicode=0 stretchH=100 smooth=1 aa=1 padding=0,0,0,0 spacing=0,0"
+    output.append(line)
+
+    # COMMON
+    line = "common lineHeight=" + resize(list_s[-1][1], scale) + " base=" + resize(
+        list_s[-1][1], scale) + " scaleW=0 scaleH=0 pages=" + str(len(list_t)) + " packed=0"
+    output.append(line)
+
+    # THIS IS WHERE THE FUN BEGINS
+    # PAGE
+    for item in list_t:
+        line = "page id=" + item[1] + " file=\"" + item[2].replace(".tga", ".png") + "\""
+        output.append(line)
+
+    # CHAR
+    spacing = int(resize(list_m[-1][1], scale))
+    line = "chars count=" + str(len(list_r))
+    output.append(line)
+    for item in list_r:
+        line = "char id=" + str(convert_glyph_id(int(item[1]))) + " x=" + resize(item[3], scale) + " y=" + resize(item[4], scale) + " width=" + resize(item[5], scale) + " height=" + resize(item[6],scale) + " xoffset=0 yoffset=0 xadvance=" + str(int(resize(item[5], scale)) + spacing) + " page=" + item[2] + " chnl=0"
+        output.append(line)
+
+    output.append("kernings count=0")
+
+    with open(font_name + ".fnt", 'w') as f:
+        for line in output:
+            f.write(line)
+            f.write('\n')
+
+    try:
+        subprocess.call(["magick", "mogrify", "-resize", str(scale * 100) + "%", "-format", "png", "*.tga"])
+    except FileNotFoundError:
+        print("ImageMagick not found. You'll have to convert the textures to .png yourself I guess...")
+
+
 def main():
     print("LR2FONT to FNT")
     # USER INPUT
@@ -104,7 +168,7 @@ def main():
 
 def test():
     filename = "TEST.lr2font"
-    old_converter(filename, 1)
+    convert_lr2font_to_fnt(filename, 1)
     with open("SAMPLE.fnt", 'r') as f:
         sample_file = f.read()
     with open("TEST.fnt", 'r') as f:
