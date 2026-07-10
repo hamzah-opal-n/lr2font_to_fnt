ebee2a68674f81d780fae06d612ff96975cf78f4
Hamzah "Opal" Nutt <hamzah.nutt@my.jcu.edu.au>
2026-07-11 00:10:21 +0800

    Refactor .fnt file writing function

--- lr2font_to_fnt.py
+++ lr2font_to_fnt.py
@@ -1,91 +1,28 @@
-import os
 from pathlib import Path
 import subprocess
 
 
-def convert_glyph_id(glyph_id):
-    if not (glyph_id < 256 or glyph_id > 15306):
-        if glyph_id >= 8127:
-            glyph_id += 49281
-        else:
-            glyph_id += 32832
-    glyph_id = glyph_id.to_bytes((glyph_id.bit_length() + 7) // 8, 'big')
-    return ord(glyph_id.decode('cp932'))
-
-
-def resize_old(thing, scale):
-    thing = round(float(thing) * scale)
-    return str(thing)
-
-
-def scale_value(value, scale):
-    return round(value * scale)
-
-
-def old_converter(fileName, scale):
-    # LOAD LR2FONT AND SORT TAGS
-    inputFile = open(os.path.expanduser(fileName), "r", errors="ignore")
-    listS = []
-    listM = []
-    listT = []
-    listR = []
-    output = []
-    fontName = fileName.replace(".lr2font", "")
-
-    while True:
-        line = inputFile.readline()
-        if not line:
-            break
-        if line[0:2] == "#S":
-            listS.append(line.strip().split(","))
-        elif line[0:2] == "#M":
-            listM.append(line.strip().split(","))
-        elif line[0:2] == "#T":
-            listT.append(line.strip().split(","))
-        elif line[0:2] == "#R":
-            listR.append(line.strip().split(","))
-    inputFile.close()
-
-    # INFO
-    line = "info face=\"" + fontName + "\" size=" + resize_old(
-        listS[-1][1],
-        scale) + " bold=0 italic=0 charset=\"\" unicode=0 stretchH=100 smooth=1 aa=1 padding=0,0,0,0 spacing=0,0"
-    output.append(line)
-
-    # COMMON
-    line = "common lineHeight=" + resize_old(listS[-1][1], scale) + " base=" + resize_old(
-        listS[-1][1], scale) + " scaleW=0 scaleH=0 pages=" + str(len(listT)) + " packed=0"
-    output.append(line)
-
-    # THIS IS WHERE THE FUN BEGINS
-    # PAGE
-    for item in listT:
-        line = "page id=" + item[1] + " file=\"" + item[2].replace(".tga", ".png") + "\""
-        output.append(line)
-
-    # CHAR
-    spacing = int(resize_old(listM[-1][1], scale))
-    line = "chars count=" + str(len(listR))
-    output.append(line)
-    for item in listR:
-        line = "char id=" + str(convert_glyph_id(int(item[1]))) + " x=" + resize_old(item[3],
-                                                                                     scale) + " y=" + resize_old(
-            item[4], scale) + " width=" + resize_old(item[5], scale) + " height=" + resize_old(item[6],
-                                                                                               scale) + " xoffset=0 yoffset=0 xadvance=" + str(
-            int(resize_old(item[5], scale)) + spacing) + " page=" + item[2] + " chnl=0"
-        output.append(line)
-
-    output.append("kernings count=0")
-
-    with open(fontName + ".fnt", 'w') as f:
-        for line in output:
-            f.write(line)
-            f.write('\n')
-
+def old_magick_code(scale):
     try:
         subprocess.call(["magick", "mogrify", "-resize", str(scale * 100) + "%", "-format", "png", "*.tga"])
     except FileNotFoundError:
         print("ImageMagick not found. You'll have to convert the textures to .png yourself I guess...")
+
+
+def convert_char_id(char_id):
+    """Convert .lr2font glyph id to standard glyph id for .fnt files"""
+    if not (char_id < 256 or char_id > 15306):
+        if char_id >= 8127:
+            char_id += 49281
+        else:
+            char_id += 32832
+    char_id = char_id.to_bytes((char_id.bit_length() + 7) // 8, 'big')
+    return ord(char_id.decode('cp932'))
+
+
+def scale_value(value, scale):
+    """Scale value based on scale factor"""
+    return round(value * scale)
 
 
 def load_lr2font(lr2font_filepath):
@@ -107,55 +44,6 @@ def load_lr2font(lr2font_filepath):
                     {"char": int(record[1]), "img": int(record[2]), "x": int(record[3]), "y": int(record[4]),
                      "w": int(record[5]), "h": int(record[6])})
     return lr2font
-
-
-def convert_lr2font_to_fnt(filepath_string, scale):
-    """Convert .lr2font file to .fnt file"""
-    lr2font_filepath = Path(filepath_string)
-    lr2font = load_lr2font(lr2font_filepath)
-    font_name = lr2font_filepath.stem
-    fnt = create_fnt_from_lr2font(lr2font, scale)
-    print(fnt)
-    create_and_write_fnt_old(lr2font, font_name, scale)
-
-
-def create_and_write_fnt_old(lr2font, font_name, scale):
-    output = []
-
-    # INFO
-    line = "info face=\"" + font_name + "\" size=" + resize_old(
-        lr2font["#S"][-1]["size"],
-        scale) + " bold=0 italic=0 charset=\"\" unicode=0 stretchH=100 smooth=1 aa=1 padding=0,0,0,0 spacing=0,0"
-    output.append(line)
-
-    # COMMON
-    line = "common lineHeight=" + resize_old(lr2font["#S"][-1]["size"], scale) + " base=" + resize_old(
-        lr2font["#S"][-1]["size"], scale) + " scaleW=0 scaleH=0 pages=" + str(len(lr2font["#T"])) + " packed=0"
-    output.append(line)
-
-    # THIS IS WHERE THE FUN BEGINS
-    # PAGE
-    for item in lr2font["#T"]:
-        line = "page id=" + item["img"] + " file=\"" + item["file"].replace(".tga", ".png") + "\""
-        output.append(line)
-
-    # CHAR
-    spacing = int(resize_old(lr2font["#M"][-1]["spacing"], scale))
-    line = "chars count=" + str(len(lr2font["#R"]))
-    output.append(line)
-    for item in lr2font["#R"]:
-        line = "char id=" + str(convert_glyph_id(int(item["char"]))) + " x=" + resize_old(item["x"],
-                                                                                          scale) + " y=" + resize_old(
-            item["y"], scale) + " width=" + resize_old(item["w"], scale) + " height=" + resize_old(item["h"],
-                                                                                                   scale) + " xoffset=0 yoffset=0 xadvance=" + str(
-            int(resize_old(item["w"], scale)) + spacing) + " page=" + item["img"] + " chnl=0"
-        output.append(line)
-
-    output.append("kernings count=0")
-    with open(f"{font_name}.fnt", 'w') as f:
-        for line in output:
-            f.write(line)
-            f.write('\n')
 
 
 def create_fnt_from_lr2font(lr2font, scale):
@@ -212,7 +100,7 @@ def create_fnt_from_lr2font(lr2font, scale):
     # char
     for item in lr2font["#R"]:
         fnt["char"].append({
-            "id": convert_glyph_id(item["char"]),
+            "id": convert_char_id(item["char"]),
             "x": scale_value(item["x"], scale),
             "y": scale_value(item["y"], scale),
             "width": scale_value(item["w"], scale),
@@ -227,28 +115,49 @@ def create_fnt_from_lr2font(lr2font, scale):
     return fnt
 
 
+def write_fnt_file(fnt, fnt_filepath):
+    """Write .fnt file to fnt_filepath"""
+    output = []
+    for name, records in fnt.items():
+        for record in records:
+            line = [name]
+            for key, value in record.items():
+                line.append(f"{key}={value}")
+            output.append(" ".join(line))
+    with open(fnt_filepath, "w") as f:
+        f.write("\n".join(output))
+
+
+def convert_lr2font_to_fnt(filepath_string, scale):
+    """Convert .lr2font file to .fnt file"""
+    lr2font_filepath = Path(filepath_string)
+    lr2font = load_lr2font(lr2font_filepath)
+    fnt = create_fnt_from_lr2font(lr2font, scale)
+    write_fnt_file(fnt, lr2font_filepath.with_suffix(".fnt"))
+
+
 def main():
     print("LR2FONT to FNT")
     # USER INPUT
     while True:
-        receivedInput = input("Enter lr2font filename: ")
+        received_input = input("Enter lr2font filename: ")
         try:
-            fileName = str(receivedInput)
+            file_name = str(received_input)
         except ValueError:
             print("Invalid input received.")
         else:
             break
 
     while True:
-        receivedInput = input("Enter scaling factor (decimal): ")
+        received_input = input("Enter scaling factor (decimal): ")
         try:
-            scale = float(receivedInput)
+            scale = float(received_input)
         except ValueError:
             print("Invalid input received.")
         else:
             break
 
-    old_converter(fileName, scale)
+    convert_lr2font_to_fnt(file_name, scale)
 
 
 def test():
@@ -264,4 +173,5 @@ def test():
         print("TEST FAILED")
 
 
-test()
+# test()
+main()
