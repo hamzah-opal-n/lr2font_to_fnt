382bcf8b9049ae6ed7b0c01d664a6bbf1785d5b1
Hamzah "Opal" Nutt <hamzah.nutt@my.jcu.edu.au>
2026-07-10 18:45:43 +0800

    Refactor .fnt creation function

--- lr2font_to_fnt.py
+++ lr2font_to_fnt.py
@@ -68,7 +68,11 @@ def old_converter(fileName, scale):
     line = "chars count=" + str(len(listR))
     output.append(line)
     for item in listR:
-        line = "char id=" + str(convert_glyph_id(int(item[1]))) + " x=" + resize_old(item[3], scale) + " y=" + resize_old(item[4], scale) + " width=" + resize_old(item[5], scale) + " height=" + resize_old(item[6], scale) + " xoffset=0 yoffset=0 xadvance=" + str(int(resize_old(item[5], scale)) + spacing) + " page=" + item[2] + " chnl=0"
+        line = "char id=" + str(convert_glyph_id(int(item[1]))) + " x=" + resize_old(item[3],
+                                                                                     scale) + " y=" + resize_old(
+            item[4], scale) + " width=" + resize_old(item[5], scale) + " height=" + resize_old(item[6],
+                                                                                               scale) + " xoffset=0 yoffset=0 xadvance=" + str(
+            int(resize_old(item[5], scale)) + spacing) + " page=" + item[2] + " chnl=0"
         output.append(line)
 
     output.append("kernings count=0")
@@ -86,20 +90,22 @@ def old_converter(fileName, scale):
 
 def load_lr2font(lr2font_filepath):
     """Load .lr2font file and return info as dict"""
-    lr2font = {"#S": [], "#M": [], "#T": [], "#R": []}
+    lr2font = {"name": lr2font_filepath.stem, "#S": [], "#M": [], "#T": [], "#R": []}
     with lr2font_filepath.open(errors="ignore") as input_file:
         for line in input_file:
             if line[0] != "#":
                 continue
             record = line.strip().split(",")
             if record[0] == "#S":  # S lines - define font size
-                lr2font["#S"].append({"size": record[1]})
+                lr2font["#S"].append({"size": int(record[1])})
             elif record[0] == "#M":  # M lines - define font spacing
-                lr2font["#M"].append({"spacing": record[1]})
+                lr2font["#M"].append({"spacing": int(record[1])})
             elif record[0] == "#T":  # T lines - define image files
-                lr2font["#T"].append({"img": record[1], "file": record[2]})
+                lr2font["#T"].append({"img": int(record[1]), "file": record[2]})
             elif record[0] == "#R":  # R lines - define characters
-                lr2font["#R"].append({"char": record[1], "img": record[2], "x": record[3], "y": record[4], "w": record[5], "h": record[6]})
+                lr2font["#R"].append(
+                    {"char": int(record[1]), "img": int(record[2]), "x": int(record[3]), "y": int(record[4]),
+                     "w": int(record[5]), "h": int(record[6])})
     return lr2font
 
 
@@ -107,8 +113,14 @@ def convert_lr2font_to_fnt(filepath_string, scale):
     """Convert .lr2font file to .fnt file"""
     lr2font_filepath = Path(filepath_string)
     lr2font = load_lr2font(lr2font_filepath)
-    output = []
     font_name = lr2font_filepath.stem
+    fnt = create_fnt_from_lr2font(lr2font, scale)
+    print(fnt)
+    create_and_write_fnt_old(lr2font, font_name, scale)
+
+
+def create_and_write_fnt_old(lr2font, font_name, scale):
+    output = []
 
     # INFO
     line = "info face=\"" + font_name + "\" size=" + resize_old(
@@ -132,15 +144,87 @@ def convert_lr2font_to_fnt(filepath_string, scale):
     line = "chars count=" + str(len(lr2font["#R"]))
     output.append(line)
     for item in lr2font["#R"]:
-        line = "char id=" + str(convert_glyph_id(int(item["char"]))) + " x=" + resize_old(item["x"], scale) + " y=" + resize_old(item["y"], scale) + " width=" + resize_old(item["w"], scale) + " height=" + resize_old(item["h"], scale) + " xoffset=0 yoffset=0 xadvance=" + str(int(resize_old(item["w"], scale)) + spacing) + " page=" + item["img"] + " chnl=0"
+        line = "char id=" + str(convert_glyph_id(int(item["char"]))) + " x=" + resize_old(item["x"],
+                                                                                          scale) + " y=" + resize_old(
+            item["y"], scale) + " width=" + resize_old(item["w"], scale) + " height=" + resize_old(item["h"],
+                                                                                                   scale) + " xoffset=0 yoffset=0 xadvance=" + str(
+            int(resize_old(item["w"], scale)) + spacing) + " page=" + item["img"] + " chnl=0"
         output.append(line)
 
     output.append("kernings count=0")
-
-    with open(font_name + ".fnt", 'w') as f:
+    with open(f"{font_name}.fnt", 'w') as f:
         for line in output:
             f.write(line)
             f.write('\n')
+
+
+def create_fnt_from_lr2font(lr2font, scale):
+    """Create .fnt dict from .lr2font dict with values adjusted based on scale"""
+    font_size = scale_value(lr2font["#S"][-1]["size"], scale)
+    spacing = scale_value(lr2font["#M"][-1]["spacing"], scale)
+    fnt = {
+        "info": [
+            {
+                "face": f"\"{lr2font['name']}\"",
+                "size": font_size,
+                "bold": 0,
+                "italic": 0,
+                "charset": "\"\"",
+                "unicode": 0,
+                "stretchH": 100,
+                "smooth": 1,
+                "aa": 1,
+                "padding": "0,0,0,0",
+                "spacing": "0,0"
+            }
+        ],
+        "common": [
+            {
+                "lineHeight": font_size,
+                "base": font_size,
+                "scaleW": 0,
+                "scaleH": 0,
+                "pages": len(lr2font["#T"]),
+                "packed": 0
+            }
+        ],
+        "page": [],
+        "chars": [
+            {
+                "count": len(lr2font["#R"])
+            }
+        ],
+        "char": [],
+        "kernings": [
+            {
+                "count": 0
+            }
+        ]
+    }
+
+    # page
+    for item in lr2font["#T"]:
+        fnt["page"].append({
+            "id": item["img"],
+            "file": f"\"{item['file'].replace('.tga', '.png')}\""
+        })
+
+    # char
+    for item in lr2font["#R"]:
+        fnt["char"].append({
+            "id": convert_glyph_id(item["char"]),
+            "x": scale_value(item["x"], scale),
+            "y": scale_value(item["y"], scale),
+            "width": scale_value(item["w"], scale),
+            "height": scale_value(item["h"], scale),
+            "xoffset": 0,
+            "yoffset": 0,
+            "xadvance": scale_value(item["w"], scale) + spacing,
+            "page": item["img"],
+            "chnl": 0
+        })
+
+    return fnt
 
 
 def main():
